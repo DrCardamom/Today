2249ff8256b4aad315659a8b8959021a67edcbe9
DrCardamom <erika.shimba@400f.jp>
2022-12-23 08:02:48 +0900

    All edited files

---
+++ Today/ListViewController/ReminderListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Erika Shimba on 2022/12/20.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    var dataSource: DataSource!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration (handler: cellRegisrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        dataSource.apply(snapshot)
+        
+        collectionView.dataSource = dataSource
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
